f74f3a11388c222732703afde85ea27e2adbc419
akash vaidya <99950008+Vaidyaakash@users.noreply.github.com>
2022-05-24 16:05:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import Contact from './Pages/Contact'
 import Home from './Pages/Home'
 import Nav from './Pages/Nav'
 import Student from './Pages/Student'
-import "./Style.css"
+import Homepage from './Pages/Homepage'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import "./Style.css"
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
 
-      <Route path='/' element = {<Nav />} />
+      <Route path='/' element = {<Homepage />} />
       <Route path='/homepage' element = {<Home />} />
       <Route path='/student' element = {<Student />} />
       <Route path='/contact' element = {<Contact />} />
