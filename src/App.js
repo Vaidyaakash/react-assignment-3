9c1fc9f30e1d0fceebf1b56d82556aab33675c23
Vaidyaakash <akashvaidya132@gmail.com>
2022-05-24 15:41:33 +0530

    assignment 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Contact from './Pages/Contact'
+import Home from './Pages/Home'
+import Nav from './Pages/Nav'
+import Student from './Pages/Student'
+import "./Style.css"
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <Routes>
+
+      <Route path='/' element = {<Nav />} />
+      <Route path='/homepage' element = {<Home />} />
+      <Route path='/student' element = {<Student />} />
+      <Route path='/contact' element = {<Contact />} />
+    </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
